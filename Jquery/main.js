d9188b9f9e8d12bf577ac87e0933168da4f62953
Ciprus-ro <74007528+Ciprus-ro@users.noreply.github.com>
2021-01-09 21:34:39 +0200

    up

---
+++ Jquery/main.js
@@ -0,0 +1,21 @@
+ 
+let thePets = [
+    {
+    'name': 'Tom',
+    'color': 'grey',
+    'numbFeet': 4,
+    'favFood':  'fruits',
+    },
+    {
+    'name': 'Kitty',
+    'color': 'black',
+    'numbFeet': 4,
+    'favFood':  'meat',
+    },
+    {
+    'name': 'Angela Merkel',
+    'color': 'yellow',
+    'numbFeet': 4,
+    'favFood':  'all',
+    }
+]
